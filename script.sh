28b78bdb01eb4fa88bb58e134f832a9330329678
sahil7023 <43112428+sahil7023@users.noreply.github.com>
2024-11-10 22:08:08 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #update the ubuntu machine
-sudo apt update
+sudo apt update -y
 
 #install nodejs in the machine
-sudo apt install nodejs
+sudo apt install nodejs -y
 
 #install npm in the machine
-sudo apt install npm
+sudo apt install npm -y
 
 #clone the repository
 git clone https://github.com/sahil7023/aws-ec2-script-for-launching-instances.git /home/ubuntu/project
@@ -16,7 +16,7 @@ git clone https://github.com/sahil7023/aws-ec2-script-for-launching-instances.gi
 cd /home/ubuntu/project
 
 #install packages
-sudo npm install
+sudo npm install -y
 
 #turn up the server
 sudo npx pm2 start index.js
